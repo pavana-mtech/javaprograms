d0f16bc5ba0bd59408c6bd18af221cc5d5652e18
pavana-mtech <56587429+pavana-mtech@users.noreply.github.com>
2019-12-03 16:14:50 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,14 @@
+class Program1
+ {
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Program starts............");
+		int a = 100,b = 200,c = a + b;
+		System.out.println("the value of a is "+a);
+		System.out.println("the value of b is "+b);
+		System.out.println("sum of a and b is "+c);
+		System.out.println("Program ends.......");
+	}
+	
+}
